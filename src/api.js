8036ef8ea4d4ca22cfa3c5aeab8e37db2dd5c5d6
掉漆 <diaoq@reachauto.com>
2024-10-15 14:36:36 +0800

    init

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 axios.defaults.withCredentials = true;
 const instance = axios.create({
-  baseURL: 'https://er.767766.xyz/reach', // 此处设置baseURL
+  baseURL: 'https://test.domain.com/reach', // 此处设置baseURL
   withCredentials: true,
   timeout: 50000,
 });
